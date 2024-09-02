b55697657de21dfde1ae136a1279881ad437f87b
mustafanahar7 <naharmustafa7@gmail.com>
2024-09-02 14:42:03 +0530

    changes in file8

--- citymart/urls.py
+++ citymart/urls.py
@@ -20,6 +20,7 @@ from Appone import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.SplashPage,name="splashpage"),
@@ -42,5 +43,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
