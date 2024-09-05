a46581c0e4cc7b6685a1f4583a9031acf1abae2c
mustafanahar7 <naharmustafa7@gmail.com>
2024-09-05 12:56:42 +0530

    change files15

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,50 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
   const cartItemsContainer = document.getElementById("cart-items");
   const totalAmountEl = document.getElementById("total-amount");
-  const totalMrpEl = document.getElementById("total-mrp-amount");
   const discountAmountEl = document.getElementById("discount-amount");
   const finalAmountEl = document.getElementById("final-amount");
   const totalAmountInput = document.getElementById("grand-total");
 
   function calculateTotal() {
     let total = 0;
-    let totalMrp = 0;
-    let discount = 0;
-    let finalAmount = 0;
-
-    // Iterate over each cart item to calculate the total amounts
     document.querySelectorAll(".cart-item").forEach((row) => {
       const quantity = parseInt(row.querySelector(".qty").textContent, 10);
       const price = parseFloat(row.querySelector(".price").textContent);
-      const mrp = parseFloat(row.querySelector(".mrp").textContent);
+      // const mrp = document.getElementById("mrp");
+
+      // console.log(mrp);
 
       const itemTotal = quantity * price;
-      const mrpItemTotal = quantity * mrp;
-
-      // Update the total and mrp amounts for the item
       row.querySelector(".item-total-amount").textContent =
         itemTotal.toFixed(2);
-
       total += itemTotal;
-      totalMrp += mrpItemTotal;
     });
-
-    // Calculate discount (Total MRP - Total Selling Price)
-    discount = totalMrp - total;
-
-    // Calculate the final bill amount
-    finalAmount = total;
-
-    // Update the DOM elements with calculated values
-    totalMrpEl.textContent = totalMrp.toFixed(2);
-    discountAmountEl.textContent = discount.toFixed(2);
-    finalAmountEl.textContent = finalAmount.toFixed(2);
-    totalAmountEl.textContent = finalAmount.toFixed(2);
-    totalAmountInput.value = finalAmount.toFixed(2);
-
-    console.log(
-      `Total MRP: ${totalMrp}, Discount: ${discount}, Final Amount: ${finalAmount}`
-    );
+    totalAmountEl.textContent = total.toFixed(2);
+    totalAmountInput.value = total.toFixed(2);
+    console.log(totalAmountInput.value);
   }
 
   cartItemsContainer.addEventListener("click", function (event) {
